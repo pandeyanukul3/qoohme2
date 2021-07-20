3de2e97fb1ee99383aff18171c59f4ab8fd3298b
pandeyanukul3 <79939987+pandeyanukul3@users.noreply.github.com>
2021-07-21 00:26:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const client = require('twilio')('AC335598c1b43cfc21fe200f4c9daa885f', '2dca4e2e0a454532c3020cafa7991af0', { 
+    lazyLoading: true 
+  });
+  
+app.get('/',(req,res)=>{
+    client.messages
+    .create({
+       body: req.headers,
+       from: 'whatsapp:+14155238886',
+       to: 'whatsapp:+917489170399'
+     })
+    .then(message => console.log(message.sid));
+    res.redirect('http://www.m.qooh.me/Veevaa');
+})
+app.listen(process.env.PORT,()=>console.log(`Listening on http://localhost:${process.env.PORT}`))
